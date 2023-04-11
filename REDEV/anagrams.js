10f20c164221c61941604a4c53ba75c60fc4329c
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-04-11 14:49:17 +0300

    commit

---
+++ REDEV/anagrams.js
@@ -0,0 +1,18 @@
+function anagrams(word, arr) {
+
+    let arrOfWord = word.split('').sort().join('')
+    let finalArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        let wordFromArray = arr[i].split('').sort().join('')
+        if (arrOfWord === wordFromArray) {
+            finalArr.push(arr[i])
+        }
+    }
+    return finalArr
+}
+
+
+console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
+
+//anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) --> ['aabb', 'bbaa']
