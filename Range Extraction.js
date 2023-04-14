45e1447f73ca7ba030afd4e5634fea81512ed3b4
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-04-14 21:38:40 +0300

    commit

---
+++ Range Extraction.js
@@ -0,0 +1,21 @@
+function solution(list) {
+    let arr = []
+
+    for (let i = 0; i < list.length; i++) {
+
+        let j = i
+        while (list[j] === list[j+1] - 1) j++
+        if (j != i && j-i>1) {
+
+            list.splice(i, j-i+1, list[i] +'-'+list [j])
+        }
+
+    }
+    return list.join(', ')
+
+}
+
+
+
+
+console.log(solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
