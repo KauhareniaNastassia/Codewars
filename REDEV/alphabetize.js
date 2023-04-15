9e898186f4fd729a04ce1befa7841672161a8ffc
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-04-15 21:45:22 +0300

    commit

---
+++ REDEV/alphabetize.js
@@ -0,0 +1,13 @@
+/*
+Напишите функцию `alphabetize(str)`, которая возвращает строку,
+    отсортировав её символы в алфавитном порядке.
+
+    Например:
+alphabetize("redev") --> "deerv"*/
+
+
+function alphabetize(str) {
+    return  str.split('').sort().join('')
+}
+
+console.log(alphabetize('redev'))
