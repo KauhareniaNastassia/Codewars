5297aa8eefe5fe31707b03826d9636d3f74ed0c1
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-04-17 17:43:22 +0300

    commit

--- Roman Numerals Decoder.js
+++ Roman Numerals Decoder.js
@@ -1,13 +1,36 @@
 function solution(roman) {
-    let numeralCodes = [
-                        ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],         // Hundreds
-                        ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],          // Tens
-                        ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]];        // Ones
+    const rom = {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        X: 10,
+        V: 5,
+        I: 1
+    }
+    let arr = Object.entries(rom)
+    let romanArr = roman.split('')
+    let count = 0
 
+    for (let i = 0; i < romanArr.length; i++) {
 
+        for (let j = 0; j < arr.length; j++) {
+
+            let el = arr[j]
+            if (romanArr[i] === el[0]) {
+                if (arr[j] < arr[j+1]) {
+                    count += Number(arr[j+1][1] - arr[j][1] )
+                } else {
+                    count += Number(el[1])
+                }
+            }
+        }
+    }
+    return count
 }
 
 
-console.log(solution('XXI'))
-console.log(solution('MMVIII'))
-console.log(solution('MDCLXVI'))
+console.log(solution('IV'))
+//console.log(solution('XXI'))
+//console.log(solution('MMVIII'))
+//console.log(solution('MDCLXVI'))
